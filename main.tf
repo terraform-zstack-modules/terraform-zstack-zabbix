d8f7f40d614bc0f774da2af1bac637c9b64de5fe
jiajian.chi <jiajian.chi@zstack.io>
2025-04-03 16:30:32 +0800

    change image and instance module url

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ locals {
 
 
 module "zabbix_image" {
-  source = "git::https://github.com/terraform-zstack-modules/terraform-zstack-image.git"
+  source = "git::http://172.20.14.17/jiajian.chi/terraform-zstack-image.git"
 
   create_image        = true
   image_name          = var.image_name
@@ -21,7 +21,7 @@ module "zabbix_image" {
 
 # 创建虚拟机实例
 module "zabbix_instance" {
-  source = "git::https://github.com/chijiajian/terraform-zstack-instance.git"
+  source = "git::http://172.20.14.17/jiajian.chi/terraform-zstack-instance.git"
 
   name                  = var.instance_name
   description           = "zabbix Server Created by Terraform"
